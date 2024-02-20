802b0af5acf028809dae0b834cb49a576c77533e
Michael Edelman <mike.edelman@gmail.com>
2024-02-20 18:19:07 -0500

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,47 @@ import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "James Murphy and the Giant Fishing Expedition";
+const description =
+  "The story of Atlantic Highlands, NJ -- a small town that faces big challenges when a losing mayoral candidate questions his constituents right to vote -- leading to a community-wide discussion about honesty, respect, and the importance of treating neighbors kindly";
+const url = "https://jamesmurphy.fishing";
+const name = "James Murphy and the Giant Fishing Expedition";
+
 export const metadata: Metadata = {
-  title: "James Murphy and the Giant Fishing Expedition",
-  description:
-    "The story of Atlantic Highlands, NJ -- a small town that faces big challenges when a losing mayoral candidate questions his constituents right to vote -- leading to a community-wide discussion about honesty, respect, and the importance of treating neighbors kindly",
+  metadataBase: new URL(url),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    creator: "@edelman215",
+    images: ["https://jamesmurphy.fishing/jimmycover.webp"],
+  },
+
+  openGraph: {
+    title,
+    description,
+    url,
+    locale: "en_US",
+    type: "book",
+    siteName: name,
+    images: [
+      {
+        url: "https://jamesmurphy.fishing/jimmycover.webp",
+        width: 800,
+        height: 600,
+        alt: "James Murphy Fishing",
+      },
+    ],
+  },
+  keywords: ["James Murphy", "Atlantic Highlands", "Election", "Fishing"],
+  title: title,
+  description: description,
 };
 
 export default function RootLayout({
